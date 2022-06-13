0b1104af19f03d373da14b8945f732dd8d49b31e
Michael Schoenecker <michael.schoenecker@gmail.com>
2022-06-13 18:25:38 -0400

    refactor to ES6 classes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,43 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+//import exampleVideoData from 'exampleVideoData.js';
+import Search from '/compiled/src/components/Search.js'; //'Search.jsx';
+//import defaultExport from "module-name";
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+  }
+
+  render() {
+
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><Search /><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><em>videoPlayer</em> view goes here</h5></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><em>videoList</em> view goes here</h5></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+
+  }
+
+
+}
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+ReactDOM.render(<App />, document.getElementById('app'));
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,18 +1,32 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+//import exampleVideoData from 'exampleVideoData.js';
 
-// PropTypes tell other developers what `props` a component expects
-// Warnings will be shown in the console when the defined rules are violated
-VideoList.propTypes = {
-  videos: PropTypes.array.isRequired
-};
+class VideoList extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {videos: PropTypes.array.isRequired};
+  }
+
+  render() {
+
+    return (
+      <div className="video-list">
+        <div><h5><em>videoListEntry</em> view goes here</h5></div>
+        <div><h5><em>videoListEntry</em> view goes here</h5></div>
+        <div><h5><em>videoListEntry</em> view goes here</h5></div>
+        <div><h5><em>videoListEntry</em> view goes here</h5></div>
+        <div><h5><em>videoListEntry</em> view goes here</h5></div>
+      </div>
+    );
+  }
+
+
+  // PropTypes tell other developers what `props` a component expects
+  // Warnings will be shown in the console when the defined rules are violated
+  /*VideoList.propTypes = {
+      videos: PropTypes.array.isRequired
+    };*/
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
